66e2ff4297e202290ba3e2d922d20f75b678d80d
Sarah Chok <Sarah Chok>
2016-03-30 21:58:18 +0100

    Added description to schedule. Pops out once clicked.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+$(document).ready(function() {
+
+	var toggleDescription = function() {
+		var current = $(this);
+		var closestTr = current.closest("tr");
+		var nextTr = closestTr.next("tr");
+		var foundDesc = nextTr.find(".desc");
+
+		var currentDisplay = foundDesc[0].style.display;
+
+		if(currentDisplay === ""  || currentDisplay === "none") {
+			foundDesc[0].style.display = "block";
+		} 
+		else if (currentDisplay === "block") {
+			foundDesc[0].style.display = "none";
+		}
+
+	}
+
+	$("#schedule tr").on("click", toggleDescription);
+
+});
